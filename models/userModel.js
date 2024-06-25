0d9561f1a1de062751612acb4d455281edafae0c
kobiowuquadri <kobiowuq@gmail.com>
2024-06-25 20:02:06 +0100

    add: user schema

---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { Schema } from 'mongoose'
+
+const userSchema = new Schema({
+  email : {
+    type: String,
+    required: [true, "Please provide your email."],
+    lowercase: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: [true, "Please provide your name"]
+  },
+  phoneNumber: {
+    type: String,
+    required: [true, "Please provide your phone number"]
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide your password"],
+    minLength: [6, "Please your password must not be less than 6 chracters."],
+  },
+  role: {
+    type: String, 
+    required: [true, "Please provide your role"],
+    enum: ['Beautician', 'Customer']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+export const User = mongoose.model('User', userSchema)
