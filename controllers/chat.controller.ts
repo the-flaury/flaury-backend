42fab700000f0c0ebe476a353b244b182202cea6
Austin Ameh <arewageek@gmail.com>
2024-10-31 13:21:02 +0100

    Chat api

--- controllers/chat.controller.ts
+++ controllers/chat.controller.ts
@@ -2,6 +2,7 @@ import { query, type Request, type Response } from "express";
 import { fireDB } from "../config/firebase.config";
 import { v4 as uuidv4 } from "uuid";
 import { Timestamp } from "firebase-admin/firestore";
+import { verifyAuthToken } from "../utils/verifyAuthToken";
 
 const db = fireDB;
 
@@ -42,6 +43,65 @@ export const startChat = async (req: Request, res: Response) => {
     res
       .status(400)
       .json({ success: false, message: "Message sent successfully" });
+  } catch (error: any) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+export const activeChats = async (req: Request, res: Response) => {
+  try {
+    const { authToken } = req.body;
+    const response = await verifyAuthToken(authToken);
+    if (!response.success) throw new Error(response.message);
+    const user = response.message?.id;
+
+    const chats = await db
+      .collection("inbox")
+      .where("participants", "array-contains", user);
+
+    if (!chats) throw new Error("No previous chat found");
+
+    res.status(200).json({ success: true, chats });
+  } catch (error: any) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+export const sendMessage = async (req: Request, res: Response) => {
+  const { message, sender, receiver, inboxId } = req.body;
+  try {
+    const verifyDuplicates = db
+      .collection("messages")
+      .where({ sender, message, receiver });
+
+    if (verifyDuplicates) throw new Error("Oops! You already said that");
+
+    const messageRef = await db.collection("messages").add({
+      inboxId,
+      sender,
+      receiver,
+      message,
+      isRead: false,
+      createdAt: Timestamp.now(),
+    });
+
+    if (!messageRef.id) throw new Error("Could not send message");
+
+    res.status(200).json({ success: false, message: "Message sent" });
+  } catch (error: any) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+export const loadChats = async (req: Request, res: Response) => {
+  const { inboxId } = req.body;
+  try {
+    const chats = db.collection("messages").where({ inboxId });
+
+    if (!chats)
+      res.status(404).json({ success: true, message: "No previous message" });
+
+    res.status(200).json({ success: true, message: chats });
   } catch (error: any) {
     res.status(400).json({ success: false, message: error.message });
   }
